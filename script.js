deea3198adc8e8fe160eb83c481f8488a43ed669
Shaikh Shoeb <127790182+it-Shoeb@users.noreply.github.com>
2024-07-05 00:29:06 +0530

    importing files

---
+++ script.js
@@ -0,0 +1,22 @@
+const header = document.querySelector('.navbar')
+document.body.addEventListener('scroll', (e) => {
+    console.log(e);
+    // if(e.pageY > 640){
+    //     header.style.background = 'red';
+    //     console.log(header);
+    // }  
+    // if(e.pageY > 1280){
+    //     header.style.background = 'violet';
+    //     console.log(header);
+    // }
+    // if(e.pageY > 1900){
+    //     header.style.background = 'gray';
+    //     console.log(header);
+    // }
+    // if(e.pageY > 2540){
+    //     header.style.background = 'yellow';
+    //     console.log(header);
+    // }
+    
+    // console.log(e.screenY);
+})
