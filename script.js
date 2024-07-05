3fec461f09cd3c9acd2ac0e171d6f13305ce6aee
Shaikh Shoeb <127790182+it-Shoeb@users.noreply.github.com>
2024-07-06 01:09:23 +0530

    adding 404 page and done some minor changes

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
+
 const header = document.querySelector('.navbar')
-document.body.addEventListener('scroll', (e) => {
-    console.log(e);
-    // if(e.pageY > 640){
-    //     header.style.background = 'red';
-    //     console.log(header);
-    // }  
-    // if(e.pageY > 1280){
-    //     header.style.background = 'violet';
-    //     console.log(header);
-    // }
-    // if(e.pageY > 1900){
-    //     header.style.background = 'gray';
-    //     console.log(header);
-    // }
-    // if(e.pageY > 2540){
-    //     header.style.background = 'yellow';
-    //     console.log(header);
-    // }
-    
-    // console.log(e.screenY);
+const hamburger = document.querySelector('.hamburger')
+const navLink = document.querySelector('.nav-links')
+const link = document.querySelectorAll('.link')
+
+// hamburger.addEventListener('click', () => {
+//     console.log('open');
+//     navLink.style.left=0;
+//     navLink.style.borderRadius = 0;
+//     link.forEach(element => {
+//         element.addEventListener('click', (e) => {
+//             navLink.style.left=200+'%';
+//             navLink.style.borderTopLeftRadius = 999+'px';
+//             navLink.style.borderBottomLeftRadius = 999+'px';
+//             console.log('close');
+//         })
+//     });
+// })
+
+document.addEventListener('scroll', (e) => {
+    console.log(e.body);
 })
